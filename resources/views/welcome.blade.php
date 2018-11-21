8f14398efbf8d367b6933db093eb056c715cd381
Andrew Iskarev <andgalatinov@gmail.com>
2018-11-21 20:01:52 +0300

    Login form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,50 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Rodger</title>
+    <title>Rodger</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.23/css/uikit.min.css"/>
+    <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
 
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.23/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.23/js/uikit-icons.min.js"></script>
 
-    </head>
-    <body>
-    </body>
+</head>
+<body>
+
+<div class="uk-flex uk-flex-center uk-flex-middle" style="height: 100%">
+    <div class="uk-card uk-card-default uk-card-body">
+        <h3 class="uk-card-title uk-flex uk-flex-center">Login</h3>
+            <form>
+                <div class="uk-margin">
+                    <div class="uk-inline">
+                        <span class="uk-form-icon" uk-icon="icon: user; ratio: 0.7"></span>
+                        <input class="uk-input uk-form-small" id="form-stacked-text" type="text" placeholder="Login">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <div class="uk-inline">
+                        <span class="uk-form-icon" uk-icon="icon: lock; ratio: 0.7"></span>
+                        <input class="uk-input uk-form-small" id="form-stacked-text" type="password"
+                               placeholder="Password">
+                    </div>
+                </div>
+            </form>
+           <div class="uk-flex uk-flex-center uk-flex-around">
+               <button class="uk-icon-link uk-margin-small-right" uk-icon="facebook"></button>
+               <button  class="uk-icon-link uk-margin-small-right" uk-icon="google-plus"></button>
+               <button class="uk-icon-link uk-margin-small-right" uk-icon="vimeo"></button>
+           </div>
+
+    </div>
+    </div>
+</div>
+
+</body>
 </html>
