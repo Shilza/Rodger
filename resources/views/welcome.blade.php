b5846ae5c76a85df75179d8038f1c46b70567fd9
Andrew Iskarev <andgalatinov@gmail.com>
2018-11-21 17:32:59 +0300

    Init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Rodger</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+
+    </head>
+    <body>
+    </body>
+</html>
