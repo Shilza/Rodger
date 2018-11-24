f2ef621a22213a430fcd9cfd3a3965179923e115
Toleckk <anatolich1995@gmail.com>
2018-11-24 03:48:25 +0300

    Auth with Guzzle

--- app/Authorization/AuthorizationHelper.php
+++ app/Authorization/AuthorizationHelper.php
@@ -4,111 +4,83 @@ namespace App\Authorization;
 
 
 use App\Exceptions\AuthorizationException;
-use App\Exceptions\MakeDirectoryException;
-use App\Exceptions\RequestException;
-use App\Utils\RR\HTMLParseHelper;
-use App\Utils\RR\CURLHelper;
+use App\Utils\DirectoryHelper;
+use App\Utils\RR\CookieHelper;
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Cookie\FileCookieJar;
+use GuzzleHttp\TransferStats;
 
-abstract class AuthorizationHelper{
-    const COOKIE_PATH = 'storage/app/';
+abstract class AuthorizationHelper {
+    const COOKIE_DIR = 'storage/app/cookies/';
 
-    protected $curl;
+    protected $guzzle;
+
     protected $login;
     protected $password;
-    protected $force;
+
+    protected $referer;
 
     /**
-     * @var int
+     * @var FileCookieJar
      */
-    public $accountID;
+    protected $cookies;
+    protected $cookiePath;
 
     /**
-     * @var string
-     */
-    public $cookiePath;
-
-
-    /**
-     * AuthorizationHelper constructor.
+     * NewAuthorizationHelper constructor.
      * @param string $login
      * @param string $password
+     * @param string $cookiePath
      * @param bool $force
      */
-    public function __construct(string $login, string $password, bool $force = false){
-        $this->curl = new CURLHelper();
+    public function __construct(string $login, string $password, bool $force = false, ?string $cookiePath = null) {
+        $this->guzzle = new Client([
+            'cookies' => ($this->cookies = new FileCookieJar($this->cookiePath = CookieHelper::cookiePath($cookiePath))),
+            'allow_redirects' => true,
+            'on_stats' => function (TransferStats $stats) {
+                $this->referer = $stats->getEffectiveUri() . '';
+            }
+        ]);
         $this->login = $login;
         $this->password = $password;
-        $this->force = $force;
+
+        if ($force)
+            $this->cookies->clear();
     }
 
     /**
+     * @return CookieJar
      * @throws AuthorizationException
-     * @throws MakeDirectoryException
-     * @throws RequestException
      */
-    public function authorize() : void {
-        $arr = explode(DIRECTORY_SEPARATOR, static::class);
-        $cookiePath = static::getCookieDirectory(array_pop($arr)) . "/$this->login";
-
-        $this->deleteIfExpired($cookiePath);
-        $this->logIn($cookiePath);
-
-        $this->authorizeRR($cookiePath);
+    public function authorize(): CookieJar {
+        if (!$this->checkRR()) {
+            if (!$this->checkSocial()) {
+                $this->authSocial();
+                if(!$this->checkSocial())
+                    throw new AuthorizationException('Social auth failed');
+            }
+            $this->authRR();
+            if(!$this->checkRR())
+                throw new AuthorizationException('RR auth failed');
+        }
+        $this->cookies->save($this->cookiePath);
+        return $this->cookies;
     }
 
-    /**
-     * @param string $cookiePath
-     */
-    abstract public function logIn(string $cookiePath) : void;
-
-    /**
-     * @param string $cookiePath
-     * @throws AuthorizationException
-     * @throws RequestException
-     * @throws MakeDirectoryException
-     */
-    abstract protected function authorizeRR(string $cookiePath) : void;
-
-    /**
-     * @param $type
-     * @return string
-     * @throws MakeDirectoryException
-     */
-    protected static function getCookieDirectory(string $type = 'RR') : string{
-        $path = static::COOKIE_PATH . DIRECTORY_SEPARATOR . $type;
-        if(!(file_exists($path) || mkdir($path, 0777, true)))
-            throw new MakeDirectoryException();
-
-        return $path;
+    private function checkRR(): bool {
+        return
+            str_contains(($this->guzzle->get('https://rivalregions.com')->getBody() . ''), 'c_html');
     }
 
-    /**
-     * @param $cookieRR
-     * @return bool|int
-     * @throws RequestException
-     */
-    protected function checkRRCookies(string $cookieRR){
-        if(preg_match('/var id.+;/',
-            $this->curl->get('http://rivalregions.com', $headers, 1, null, $cookieRR),
-            $matches))
-            return HTMLParseHelper::getNumeric($matches[0]);
-
-        return false;
+    protected static function cookiePath(?string $fingerprint): string {
+        return self::COOKIE_DIR
+            . (isset($fingerprint) ? $fingerprint : DirectoryHelper::getUniqueDirectoryName(self::COOKIE_DIR));
     }
 
-    /**
-     * @param string $cookiePath
-     */
-    protected function deleteIfExpired(string $cookiePath){
-        if (file_exists($cookiePath))
-            if ($this->force)
-                unlink($cookiePath);
-            else
-                try {
-                    $this->curl->get(static::COOKIE_CHECK_URL, $headers, 0,
-                        null, $cookiePath);
-                } catch (RequestException $exception) {
-                    unlink($cookiePath);
-                }
-    }
+    abstract protected function checkSocial(): bool;
+
+    abstract protected function authSocial(): void;
+
+    abstract protected function authRR(): void;
 }
--- app/Authorization/VK.php
+++ app/Authorization/VK.php
@@ -1,122 +1,51 @@
 <?php
 
 namespace App\Authorization;
-
-
-use App\Exceptions\AuthorizationException;
-use App\Exceptions\MakeDirectoryException;
-use App\Exceptions\RequestException;
 use App\Utils\RR\HTMLParseHelper as Parser;
 
-//TODO: REFACTOR
-class VK extends AuthorizationHelper
-{
-    const OAUTH_URL =
-        'https://oauth.vk.com/authorize?client_id=3524629&display=page&scope=notify,friends&redirect_uri=http://rivalregions.com/main/vklogin&response_type=code&state=';
-    const COOKIE_CHECK_URL = 'https://m.vk.com/id0';
-    const HEADERS_PARSING_REGEX = '/Location: .*access_token=(.+?)&.*viewer_id=(\d+)&.*auth_key=(.+\n)/';
+class VK extends AuthorizationHelper {
+    const OAUTH_URL = 'https://oauth.vk.com/authorize?client_id=3524629&display=page&scope=notify,friends&redirect_uri=http://rivalregions.com/main/vklogin&response_type=code&state=';
 
-    /**
-     * @param string $cookiePath
-     * @throws RequestException
-     */
-    public function logIn(string $cookiePath): void
-    {
-        if (!file_exists($cookiePath)) {
-            preg_match('/https:\/\/login.vk.com.+?"/',
-                $this->curl->get("https://m.vk.com", $headers, 0, $cookiePath), $matches);
-            $this->curl->post(Parser::deleteAll('/"/', $matches[0]), ["email" => $this->login,
-                "pass" => $this->password], $headers, 1, $cookiePath, $cookiePath);
-        }
+    protected function authSocial(): void {
+        $link = Parser::find('/https:\/\/login.vk.com.+?"/', $this->guzzle->get("https://m.vk.com")->getBody());
+        $this->guzzle->post(Parser::deleteAll('/"/', $link),
+            ['form_params' => ["email" => $this->login, "pass" => $this->password]]);
     }
 
-    /**
-     * @param string $cookiePath
-     * @throws AuthorizationException
-     * @throws RequestException
-     * @throws MakeDirectoryException
-     */
-    //TODO: REFACTOR
-    protected function authorizeRR(string $cookiePath): void
-    {
-        $id = $this->getID();
-        $cookieRR = static::getCookieDirectory() . DIRECTORY_SEPARATOR . $id;
-        if (file_exists($cookieRR) && ($this->force || $this->checkRRCookies($cookieRR) === false))
-            unlink($cookieRR);
-
-        if (!file_exists($cookieRR)) {
-            $body =
-                $this->curl->get(static::OAUTH_URL, $headers, 1, null, $cookiePath);
-
-            if (strstr($body, 'https://login.vk.com/?act=grant_access')) {
-                $this->curl->get(
-                    Parser::cut(
-                        substr($body, strpos($body, 'https://login.vk.com/?act=grant_access')), '"'),
-                    $headers, 1, null, $cookiePath, static::OAUTH_URL);
-                $parameters = $this->getParametersFromHeaders($headers);
-            } else
-                $parameters = $this->getParametersFromBody($body);
-
-            $id = $parameters['id'];
-            $accessToken = $parameters['accessToken'];
-            $hash = $parameters['hash'];
-
-            $this->curl->get(
-                "http://rivalregions.com/?id=$id&id=$id&gl_number=ru&gl_photo=&gl_photo_medium=&gl_photo_big=&tmz_sent=3&wdt_sent=1280&register_locale=ru&stateshow=&access_token=$accessToken&hash=$hash",
-                $headers, 1, $cookieRR, null,
-                "http://rivalregions.com/?api_url=http://api.vk.com/api.php&access_token=$accessToken&language=0&api_id=3201433&viewer_id=$id&user_id=2018017&stateshow=&auth_key=$hash");
-        }
-
-        if (($accountID = $this->checkRRCookies($cookieRR)) !== false) {
-            $this->accountID = $accountID;
-            $this->cookiePath = $cookieRR;
+    protected function authRR(): void {
+        $body = $this->guzzle->get(self::OAUTH_URL)->getBody();
+        if(str_contains($body, 'https://login.vk.com/?act=grant_access')){
+            $this->guzzle->get(
+                Parser::find('/(https:\/\/login.vk.com\/\?act=grant_access.*?)"/', $body, true)[1],
+                ['curl' => [CURLOPT_REFERER => static::OAUTH_URL]]
+            );
+            $params = $this->parseParamsFromReferer();
         } else
-            throw new AuthorizationException("RR authorization failed");
+            $params = $this->parseParamsFromBody($body);
+
+        $this->guzzle->get(
+            "http://rivalregions.com/?id=$params[0]&id=$params[0]&gl_number=ru&gl_photo=&gl_photo_medium=&gl_photo_big=&tmz_sent=3&wdt_sent=1280&register_locale=ru&stateshow=&access_token=$params[1]&hash=$params[2]",
+            ['curl' => [CURLOPT_REFERER => "http://rivalregions.com/?api_url=http://api.vk.com/api.php&access_token=$params[1]&language=0&api_id=3201433&viewer_id=$params[0]&user_id=2018017&stateshow=&auth_key=$params[2]"]]);
     }
 
-    /**
-     * @param string $body
-     * @return string[]
-     */
-    private function getParametersFromBody(string $body): array
-    {
-        preg_match('/name="id" value="\d+">/', $body, $matches);
-        $parameters['id'] = Parser::getNumeric($matches[0]);
-        preg_match('/name="access_token".+"/', $body, $matches);
-        preg_match('/value=".+"/', $matches[0], $matches);
-        preg_match('/".+"/', $matches[0], $matches);
-        preg_match('/[^"].+[^"]/', $matches[0], $matches);
-        $parameters['accessToken'] = trim($matches[0]);
-        preg_match('/name="hash".+"/', $body, $matches);
-        preg_match('/value=".+"/', $matches[0], $matches);
-        preg_match('/".+"/', $matches[0], $matches);
-        preg_match('/[^"].+[^"]/', $matches[0], $matches);
-        $parameters['hash'] = trim($matches[0]);
-        return $parameters;
+    private function parseParamsFromReferer(): array{
+        return [
+            Parser::find('/viewer_id=(\d+)/', $this->referer, true)[1],
+            Parser::find('/access_token=(.+?)&/', $this->referer, true)[1],
+            Parser::find('/auth_key=(.+?)$/', $this->referer, true)[1]
+        ];
     }
 
-    /**
-     * @param string $headers
-     * @return string[]
-     */
-    private function getParametersFromHeaders(string $headers): array
-    {
-        preg_match(static::HEADERS_PARSING_REGEX, $headers, $matches);
-        return ['accessToken' => $matches[1], 'id' => $matches[2], 'hash' => trim($matches[3])];
+    private function parseParamsFromBody(string $body): array{
+        return [
+            Parser::find('/name="id" value="(\d+)"/', $body, true)[1],
+            Parser::find('/name="access_token" value="(.*?)"/', $body, true)[1],
+            Parser::find('/name="hash" value="(.*?)"/', $body, true)[1]
+        ];
     }
 
-    /**
-     * @return null|string|string[]
-     * @throws AuthorizationException
-     * @throws MakeDirectoryException
-     */
-    private function getID()
-    {
-        $cookie = file_get_contents(static::getCookieDirectory('VK')
-            . DIRECTORY_SEPARATOR . $this->login);
-        if(!preg_match('/\tl\t\d+/', $cookie, $matches))
-            throw new AuthorizationException('VK authorization failed');
-
-        return Parser::getNumeric($matches[0]);
+    protected function checkSocial(): bool {
+        return !is_null(Parser::find('/https:\/\/login\.vk\.com\/\?act=logout_mobile/',
+            $this->guzzle->get('https://m.vk.com/id0')->getBody() . ''));
     }
 }
