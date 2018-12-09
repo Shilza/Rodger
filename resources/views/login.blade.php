cb00f6d0aa931dd61dd117f90adb53513d044d9c
Shilza <andgalatinov@gmail.com>
2018-12-09 17:33:42 +0300

    UIKit icons link changed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,7 +19,7 @@
 
     <!-- UIkit JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.23/js/uikit.min.js"></script>
-    <script src="uikit/dist/js/uikit-icons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.23/js/uikit-icons.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
